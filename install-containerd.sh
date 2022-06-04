478e26c18415851761aae01cd8ddcfc46cba40c3
Paolo Lambojon <paololambojon@gmail.com>
2022-06-04 18:17:33 +0800

    [2] Updated the version of the kubelet, kubeadm, and kubectl to 1.21.0-00 for installation

--- install-containerd.sh
+++ install-containerd.sh
@@ -10,7 +10,7 @@ sudo modprobe br_netfilter
 
 #Setup required sysctl params,these persist across reboots.
 
-cat << EOF|sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
@@ -22,8 +22,8 @@ sudo sysctl --system
 sudo apt-get update
 sudo apt-get install -y containerd
 
-sudo mkdir -p/etc/containerd
-containerd config default|sudo tee /etc/containerd/config.toml
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
 
 sudo systemctl restart containerd
 service containerd status
